04d2dd1644993ffcf00ffbaa4e0d781ebd3c44cc
章胖胖 <375532103@qq.com>
2019-12-06 21:55:52 +0800

    axios请求优化

--- admin/src/plugin/axios/index.js
+++ admin/src/plugin/axios/index.js
@@ -36,6 +36,25 @@ const service = axios.create({
 // 解决跨域session丢失问题
 service.defaults.withCredentials = true
 
+// 存储的HTTP请求
+let pending = []
+let cancelToekn = axios.CancelToken
+let removePending = (config, isAll) => {
+    for (let i = 0; i < pending.length; i++) {
+        if (isAll) {
+            pending[i].fun()
+            pending.splice(i, 1)
+            i--
+        } else {
+            if (pending[i].name == config.url + JSON.stringify(config.data) + '&' + config.method) {
+                pending[i].fun()
+                pending.splice(i, 1)
+                break
+            }
+        }
+    }
+}
+
 // 请求拦截器
 service.interceptors.request.use(
     config => {
@@ -54,6 +73,19 @@ service.interceptors.request.use(
                 duration: 3 * 1000
             }))
         }
+
+         // 在HTTP请求前取消前面的所有请求
+        if (config.headers.removeP) removePending(config, true)
+         // 在HTTP请求前取消相同的请求
+        else removePending(config, false)
+
+        // 记录本次HTTP请求
+        config.cancelToken = new cancelToekn((c) => {
+            pending.push({
+                name: config.url + JSON.stringify(config.data) + '&' + config.method,
+                fun: c
+            })
+        })
 
         // 在请求发送之前做一些处理
         const token = util.cookies.get('token')
@@ -78,6 +110,11 @@ service.interceptors.request.use(
 // 响应拦截器
 service.interceptors.response.use(
     response => {
+        setTimeout(() => {
+            // 请求成功后删除记录（延时是为了防止短时间内重复请求）
+            removePending(response.config)
+        }, 500)
+
         // dataAxios 是 axios 返回数据中的 data
         const dataAxios = response.data
         // 这个状态码是和后端约定的
@@ -103,12 +140,10 @@ service.interceptors.response.use(
                 case 403:
                     // 无权限访问 跳转到无权限页面
                     errorCreate(dataAxios.msg)
-
                     break
                 case 404:
                     // 无权限访问 跳转到无权限页面
                     errorCreate(dataAxios.msg)
-
                     break
                 case 405:
                     // 无权限访问 跳转到无权限页面
@@ -123,23 +158,32 @@ service.interceptors.response.use(
         }
     },
     error => {
-        if (error && error.response) {
-            switch (error.response.status) {
-                case 400: error.message = '请求错误'; break
-                case 401: error.message = '未授权，请登录'; break
-                case 403: error.message = '拒绝访问'; break
-                case 404: error.message = `请求地址出错: ${error.response.config.url}`; break
-                case 408: error.message = '请求超时'; break
-                case 500: error.message = '服务器内部错误'; break
-                case 501: error.message = '服务未实现'; break
-                case 502: error.message = '网关错误'; break
-                case 503: error.message = '服务不可用'; break
-                case 504: error.message = '网关超时'; break
-                case 505: error.message = 'HTTP版本不受支持'; break
-                default: break
+        // 判断是否是被取消请求
+        if (!axios.isCancel(error)) {
+            if (error && error.response) {
+                switch (error.response.status) {
+                    case 400: error.message = '请求错误'; break
+                    case 401: error.message = '未授权，请登录'; break
+                    case 403: error.message = '拒绝访问'; break
+                    case 404: error.message = `请求地址出错: ${error.response.config.url}`; break
+                    case 408: error.message = '请求超时'; break
+                    case 500: error.message = '服务器内部错误'; break
+                    case 501: error.message = '服务未实现'; break
+                    case 502: error.message = '网关错误'; break
+                    case 503: error.message = '服务不可用'; break
+                    case 504: error.message = '网关超时'; break
+                    case 505: error.message = 'HTTP版本不受支持'; break
+                    default: break
+                }
             }
+            errorLog(error)
+
+            setTimeout(() => {
+                // 请求成功后删除记录（延时是为了防止短时间内重复请求）
+                removePending(error.config)
+            }, 500)
         }
-        errorLog(error)
+
         return Promise.reject(error)
     }
 )
