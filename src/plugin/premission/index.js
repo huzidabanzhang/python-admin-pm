86ad8b51ec12cf127a7c27d7c2eb408e7a35e716
大胡子 <375532103@qq.com>
2020-11-26 13:59:28 +0800

    新增v-default指令 下拉框指定选项不能删除

--- src/plugin/premission/index.js
+++ src/plugin/premission/index.js
@@ -2,6 +2,42 @@ import store from '@/store/index'
 
 export default {
   install (Vue, options) {
+    // 下拉框 禁止删除已选择选项或者默认选项
+    Vue.directive('default', {
+      componentUpdated (el, bindings, vnode) {
+        // values v-model 绑定值
+        // options 下拉选项
+        // prop 对应 options 中 的 value 属性
+        // defaultValue 默认值判断值
+        const [values, options, prop, defaultValue] = bindings.value
+
+        // 需要隐藏的标签索引
+        const indexs = []
+        const tempData = values.map(a => options.find(op => op[prop] === a))
+
+        tempData.forEach((a, index) => {
+          if (a[prop] === defaultValue) indexs.push(index)
+        })
+
+        const dealStyle = function (tags) {
+          tags.forEach((el, index) => {
+            if (indexs.includes(index) && ![...el.classList].includes('select-tag-close-none')) {
+              el.classList.add('none')
+            }
+          })
+        }
+
+        // 设置样式 隐藏close icon
+        const tags = el.querySelectorAll('.el-tag__close')
+        if (tags.length > 0) dealStyle(tags)
+        else {
+          setTimeout(() => {
+            dealStyle(el.querySelectorAll('.el-tag__close'))
+          })
+        }
+      }
+    })
+
     // 按钮权限判断指令
     Vue.directive('auth', function (el, binding, node) {
       let user = store.getters['chubby/user/user'],
